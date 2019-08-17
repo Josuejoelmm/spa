53463e2bf473af682b35c02d8fbd6ae7d2d59569
Josue Mendez <josuejoelmm@gmail.com>
2019-08-17 00:54:32 -0300

    trabajando en componente de formulario y notificacion

--- app/src/ui/pages/Home.js
+++ app/src/ui/pages/Home.js
@@ -1,12 +1,19 @@
 import React, { Component, Fragment } from 'react'
-import Notification from './../widgets/Notification'
+// import Notification from './../widgets/Notification'
+import Notif from '../widgets/Notif'
+import Formulario from '../widgets/Formulario'
+// import SwipeableViews from 'react-swipeable-views';
+// import { AppBar, Tabs, Tab } from '@material-ui/core'
+
+
 
 export default class Home extends Component {
     render() {
         return (
             <Fragment>
-                <h1>Hooooome</h1>
-                <Notification nuevocolor='rojo' />
+                <h1>dsa</h1>
+                <Notif />
+                <Formulario />
             </Fragment>
         )
     }
---
+++ app/src/ui/widgets/Notif.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+
+const Notificacion = (props) => {
+    return (
+        <div className={`notification-class ${props.className ? props.className : ''}`} >Notificacioooon</div>
+    );
+}
+
+export default class Notif extends Component {
+    constructor() {
+        super();
+        this.state = {
+            shouldRenderNotification : false
+        }
+        this.openNotification = this.openNotification.bind(this)
+    }
+    openNotification() {
+        this.setState({
+            shouldRenderNotification: true
+        })
+    }
+    componentWillUnmount() {
+        if (this.notificationTimeout) {
+            clearTimeout(this.notificationTimeout)
+        }
+    }
+    renderNotification() {
+        if (this.state.shouldRenderNotification) {
+            this.notificationTimeout = window.setTimeout(function() {
+                this.setState({
+                    shouldRenderNotification: false
+                });
+            }.bind(this), this.props.hideIn ? this.props.hideIn : 3000);
+            return (
+                <Notificacion 
+                    className="testing-class"
+                />
+            )
+        }
+    }
+
+    render() {
+        return (
+            <div className="notification-container">
+                <button onClick={this.openNotification} >Boton para mostrar la notificacion</button>
+            </div>
+        )
+    }
+}
